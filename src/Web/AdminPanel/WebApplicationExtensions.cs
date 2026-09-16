a292a74a04096dac120e5f310b314e52a74c07ce
Georgi Stefkoff <georgi@stefkoff.com>
2026-09-17 00:23:14 +0300

    remove item manager page (accounts->character->edit already covers item grants)

--- src/Web/AdminPanel/WebApplicationExtensions.cs
+++ src/Web/AdminPanel/WebApplicationExtensions.cs
@@ -83,7 +83,6 @@ public static class WebApplicationExtensions
         services.AddSingleton<SetupService>();
         services.AddScoped<DataUpdateService>();
         services.AddScoped<AccountService>();
-        services.AddScoped<ItemGrantService>();
         services.AddScoped<IDataService<Account>>(serviceProvider => serviceProvider.GetService<AccountService>()!);
         services.AddScoped<PlugInController>();
         services.AddScoped<IDataService<PlugInConfigurationViewItem>>(serviceProvider => serviceProvider.GetService<PlugInController>()!);
